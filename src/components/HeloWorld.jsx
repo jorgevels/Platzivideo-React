3e18d2ba54dc86cfafe6d1c863c24a8f7b02d78c
jorgevels <jorgevelasquez006@hotmail.com>
2019-11-06 10:59:24 -0500

    Primer commit

---
+++ src/components/HeloWorld.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "../assets/styles/App.scss";
+
+const HeloWorld = () => <h1>Hola Mundo</h1>;
+
+export default HeloWorld;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import HeloWorld from "./components/HeloWorld";
+
+ReactDOM.render(<HeloWorld />, document.getElementById("app"));
